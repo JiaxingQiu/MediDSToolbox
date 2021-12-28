2913ab27ad8416c680dd859096e45d40d321f056
Jiaxing Joy Qiu <jiaxingq1996@gmail.com>
2021-12-28 13:37:57 -0500

    fix the wa y to count episides in responce summary table

--- utils/front_utils/func_front_summary_tbl.R
+++ utils/front_utils/func_front_summary_tbl.R
@@ -93,12 +93,13 @@ front_summary_tbl <- function(data = subset_df(data_ml,"40w"),
     
     # factor response details
     if(dict_data$type[which(dict_data$varname==stratify_col)]=="fct"){
+      
       data$y <- data[, stratify_col]
       data$clu_col <- data[,clu_col]
       data$trim_by_col <- data[,trim_by_col]
       rsps_df <- data %>% group_by(y) %>% summarise(
         n_cluster = n_distinct(clu_col),
-        n_episode = sum(trim_by_col==(trim_vec[2]-1)*time_unit), # number of episodes counted by "anchor" 
+        n_episode = sum(diff(trim_by_col)<0), # number of episodes counted by the jump of relative time
         n_row = n()
       ) %>% as.data.frame()
     }
